11df3783f3c81986d9d1a07c7d37af03b42fe779
Al Johri <al.johri@gmail.com>
2014-08-27 22:30:19 -0400

    clean up

--- load_senate_rollcalls.py
+++ load_senate_rollcalls.py
@@ -6,31 +6,46 @@ http://www.voteview.com/senate112.htm
 
 Issue coded votes on bills
 http://voteview.com/dw-nominate_textfile.htm
-
-The format of the roll call files is:
-
- 1.  Congress Number
- 2.  ICPSR ID Number:  5 digit code assigned by the ICPSR as
-                       corrected by Howard Rosenthal and myself.
- 3.  State Code:  2 digit ICPSR State Code.
- 4.  Congressional District Number (0 if Senate)
- 5.  State Name
- 6.  Party Code:  100 = Dem., 200 = Repub. (See PARTY3.DAT)
- 7.  Occupancy:  ICPSR Occupancy Code -- 0=only occupant; 1=1st occupant; 2=2nd occupant; etc.
- 8.  Last Means of Attaining Office:  ICPSR Attain-Office Code -- 1=general election;
-                2=special election; 3=elected by state legislature; 5=appointed
- 9.  Name
-10 - to the number of roll calls + 10:  Roll Call Data --
-                             0=not a member, 1=Yea, 2=Paired Yea, 3=Announced Yea,
-                                             4=Announced Nay, 5=Paired Nay, 6=Nay,
-                             7=Present (some Congresses, also not used some Congresses),
-                             8=Present (some Congresses, also not used some Congresses),
-                             9=Not Voting
-
-example:
-1114050062 0COLORAD 10011SALAZAR    111160000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 """
 
+#### DOCUMENTATION FOR ROLL CALL VOTES
+
+#  1.  Congress Number
+#  2.  ICPSR ID Number:  5 digit code assigned by the ICPSR as
+#                        corrected by Howard Rosenthal and myself.
+#  3.  State Code:  2 digit ICPSR State Code.
+#  4.  Congressional District Number (0 if Senate)
+#  5.  State Name
+#  6.  Party Code:  100 = Dem., 200 = Repub. (See PARTY3.DAT)
+#  7.  Occupancy:  ICPSR Occupancy Code -- 0=only occupant; 1=1st occupant; 2=2nd occupant; etc.
+#  8.  Last Means of Attaining Office:  ICPSR Attain-Office Code -- 1=general election;
+#                 2=special election; 3=elected by state legislature; 5=appointed
+#  9.  Name
+# 10 - to the number of roll calls + 10:  Roll Call Data --
+#                              0=not a member, 1=Yea, 2=Paired Yea, 3=Announced Yea,
+#                                              4=Announced Nay, 5=Paired Nay, 6=Nay,
+#                              7=Present (some Congresses, also not used some Congresses),
+#                              8=Present (some Congresses, also not used some Congresses),
+#                              9=Not Voting
+
+# example:
+# 1114050062 0COLORAD 10011SALAZAR    111160000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+# Characters in roll call vote file
+# 3 - congress
+# 5 - ICPSR
+# 2 - state code
+# 1 blank
+# 1 - District  number (0 if senate)
+# 7 - state name
+# 1 blank
+# 3 - party code
+# 2 - occupancy code
+# 11 - lastname
+#rest is vector
+
+
+#### DOCUMENTATION FOR ISSUE CODED VOTES
 #  7.  Issue Code One
 #  8.  Issue Code Two
 #  9.  Month of Roll Call
@@ -47,17 +62,7 @@ example:
 # 28.  Number of 8's in Roll Call  (Present -- some Congresses, also not used some Congresses)
 # 29.  Number of 9's in Roll Call  (Not Voting)
 
-# 3 - congress
-# 5 - ICPSR
-# 2 - state code
-# 1 blank
-# 1 - District  number (0 if senate)
-# 7 - state name
-# 1 blank
-# 3 - party code
-# 2 - occupancy code
-# 11 - lastname
-#rest is vector
+
 import csv
 import numpy as np
 from blessings import Terminal
@@ -102,18 +107,16 @@ firearms_issue_code = '82'
 war_on_terror_issue_code = '108'
 
 issue_code = firearms_issue_code
+congress_number = '112'
 sum_votes_vectors = filter( lambda v: (v[7-1] == issue_code) or (v[8-1] == issue_code), bill_sum_votes )
-sum_votes_vectors = filter( lambda v: v[0] == '112', sum_votes_vectors)
+sum_votes_vectors = filter( lambda v: v[0] == congress_number, sum_votes_vectors)
 vote_indexes = map (lambda v: int(v[1]) - 1, sum_votes_vectors)
 print vote_indexes
 
+# import pdb; pdb.set_trace()
 
 
-
-import pdb; pdb.set_trace()
-
-
-
+#### Junk for later
 # for vector in bill_sum_votes:
 # 	claussen_code = vector[3-1]
 # 	peltzman_code_one = vector[4-1]
